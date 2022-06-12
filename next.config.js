586d25e7dd16369b08a040acb68d1b44c2dfe71e
maoxiaoke <maoxiaoke@outlook.com>
2022-06-12 23:29:07 +0800

    font

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  optimizeFonts: false,
+  reactStrictMode: true, // this will make page re-render
 }
 
 module.exports = nextConfig
