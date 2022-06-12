dec1a6a39faea021597b9a7066d991c36a9aed6e
maoxiaoke <maoxiaoke@outlook.com>
2022-06-12 19:07:05 +0800

    feat: init v5 successfully

--- components/HourlyTabs.tsx
+++ components/HourlyTabs.tsx
@@ -1,3 +1,6 @@
+import Canvas from '@antv/f2-react';
+import { Chart, Interval } from '@antv/f2';
+
 import Temperature from '../components/Temperature';
 export interface HourlyTabsProps {
   forcasts: Array<{
@@ -6,10 +9,24 @@ export interface HourlyTabsProps {
   }>
 }
 
+const data = [
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 120 },
+  { genre: 'Shooter', sold: 350 },
+  { genre: 'Other', sold: 150 },
+];
+
 const HourlyTabs = ({ forcasts }: HourlyTabsProps) => {
   return (
     <div>
       <p className="ml-[20px] mt-[20px] ext-lg font-bold">Today</p>
+
+      <Canvas>
+        <Chart data={data}>
+          <Interval x="genre" y="sold" />
+        </Chart>
+      </Canvas>
 
       <ul className="flex h-[160px] w-full overflow-x-scroll mt-[11px]">
         {
